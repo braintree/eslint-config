5460e1efb9dbf0cd2512735def6754e9706c3163
Blade Barringer <blade.barringer@getbraintree.com>
2020-07-15 16:05:44 +0000

    Prettier

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,7 @@ export = {
   rules: {},
   overrides: [
     {
-      files: [
-        "**/__mocks__/**/*.ts",
-        "**/__tests__/**/*.ts"
-      ],
+      files: ["**/__mocks__/**/*.ts", "**/__tests__/**/*.ts"],
       extends: "./jest",
     },
     {
