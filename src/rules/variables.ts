697d0fd7e720352f46c33fb2832ededcd9c7dfec
Blade Barringer <blade.barringer@getbraintree.com>
2020-09-11 18:21:08 +0000

    Update deps

--- src/rules/variables.ts
+++ src/rules/variables.ts
@@ -4,7 +4,6 @@ export = {
     "no-delete-var": 2,
     "no-label-var": 2,
     "no-shadow-restricted-names": 2,
-    "no-shadow": 2,
     "no-undef-init": 2,
     "no-undef": 2,
     "no-undefined": 2,
